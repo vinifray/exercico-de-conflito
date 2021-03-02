1e58f482ba311b70dd67f6fd56fa1f2042097ed2
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-03-02 13:16:33 -0300

    Ricardo added

--- src/com/br/zup/Apresentacao.java
+++ src/com/br/zup/Apresentacao.java
@@ -5,4 +5,8 @@ public class Apresentacao {
     public static void apresetaVinicius(){
         System.out.println("Oi meu nome é Vinicius");
     }
+
+    public static void apresetaRicardo(){
+        System.out.println("Oi meu nome é Ricardo");
+    }
 }
