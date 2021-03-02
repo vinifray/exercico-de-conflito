cc7060f3ac6a46e72d1d87c171a40b46fd685b3c
natalia.palota <natalia.palota@zup.com.br>
2021-03-02 13:16:15 -0300

    Adicionar apresentação Natália

--- src/com/br/zup/Apresentacao.java
+++ src/com/br/zup/Apresentacao.java
@@ -5,4 +5,8 @@ public class Apresentacao {
     public static void apresetaVinicius(){
         System.out.println("Oi meu nome é Vinicius");
     }
+
+    public static void apresentaNatalia(){
+        System.out.println("Oi meu nome é Natália");
+    }
 }
